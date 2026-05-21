6c6e54298068e444c5034a720fd49d92ae2e323d
lapotist <96067207+lapotist@users.noreply.github.com>
2026-05-21 20:47:39 +0800

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        multiset<char> ts;
+        multiset<char> cs;
+        int l = 0;
+        for(auto c : s1) {
+            ts.insert(c);
+        }
+        for(int i = 0; i<s1.length(); i++) {
+            cs.insert(s2[i]);
+        }
+        for(int r = s1.length(); r<s2.length()+1; r++) {
+            if(cs == ts) {return true;}
+            cs.erase(cs.lower_bound(s2[l]));
+            l++;
+            cs.insert(s2[r]);
+        }
+        return false;
+    }
+};
